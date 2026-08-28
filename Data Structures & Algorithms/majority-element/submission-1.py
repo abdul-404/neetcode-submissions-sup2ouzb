4bb4aaaa8570c635a6f90da5820d1716cd0fd6a7
Abdulkadir Tinwala <117512202+abdul-404@users.noreply.github.com>
2026-08-28 20:59:11 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        numdict = {}
+        for i in nums:
+            if i not in numdict:
+                numdict[i] = 1
+            else:
+                numdict[i] += 1
+        
+        highest_value = 0
+        majority_element = None
+
+        for key, value in numdict.items():
+            if value > highest_value and value > len(nums)/2:
+                highest_value = value
+                majority_element = key
+            elif value == len(nums)/2: 
+                majority_element = key
+                
+        return majority_element
